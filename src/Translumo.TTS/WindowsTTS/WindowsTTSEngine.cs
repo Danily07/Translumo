4d689458a4c35132f0f9de3a49ac1d8bfce26d06
Ivan G <ivan.g@inbox.ru>
2023-09-27 02:36:09 +0300

    speech async and stop speech when text is changed

--- src/Translumo.TTS/WindowsTTS/WindowsTTSEngine.cs
+++ src/Translumo.TTS/WindowsTTS/WindowsTTSEngine.cs
@@ -5,11 +5,19 @@ namespace Translumo.TTS.WindowsTTS;
 
 public class WindowsTTSEngine : ITTSEngine
 {
+    private SpeechSynthesizer synthesizer;
+
+    public WindowsTTSEngine()
+    {
+        this.synthesizer = new SpeechSynthesizer();
+        this.synthesizer.SetOutputToDefaultAudioDevice();
+    }
+
     public void SpeechText(string text)
     {
         // https://learn.microsoft.com/en-us/archive/msdn-magazine/2019/june/speech-text-to-speech-synthesis-in-net
-        var synthesizer = new SpeechSynthesizer();
-        synthesizer.SetOutputToDefaultAudioDevice();
+        
+        
         var builder = new PromptBuilder();
         //builder.StartVoice(new CultureInfo("en-US"));
         //builder.AppendText("All we need to do is to keep talking.");
@@ -17,6 +25,7 @@ public class WindowsTTSEngine : ITTSEngine
         builder.StartVoice(new CultureInfo("ru-RU"));
         builder.AppendText(text);
         builder.EndVoice();
-        synthesizer.Speak(builder);
+        this.synthesizer.SpeakAsyncCancelAll();
+        this.synthesizer.SpeakAsync(builder);
     }
 }
