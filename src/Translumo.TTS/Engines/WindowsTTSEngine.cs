59a1e890e051a2e909979529f04f4e4d9e2a75c1
Danil.Iushkov <danily07@yandex.ru>
2023-09-30 14:19:51 +0400

    Added user warning after language pack installation

--- src/Translumo.TTS/Engines/WindowsTTSEngine.cs
+++ src/Translumo.TTS/Engines/WindowsTTSEngine.cs
@@ -5,21 +5,27 @@ namespace Translumo.TTS.Engines;
 
 public class WindowsTTSEngine : ITTSEngine
 {
-    private readonly string _languageCode;
+    private readonly VoiceInfo _voiceInfo;
     private readonly SpeechSynthesizer _synthesizer;
 
     public WindowsTTSEngine(string languageCode)
     {
         _synthesizer = new SpeechSynthesizer();
         _synthesizer.SetOutputToDefaultAudioDevice();
-        _languageCode = languageCode;
+        _synthesizer.Rate = 1;
+
+        _voiceInfo = _synthesizer.GetInstalledVoices(new CultureInfo(languageCode)).FirstOrDefault()?.VoiceInfo;
     }
 
     public void SpeechText(string text)
     {
         // https://learn.microsoft.com/en-us/archive/msdn-magazine/2019/june/speech-text-to-speech-synthesis-in-net
+        if (_voiceInfo == null)
+        {
+            return;
+        }
         var builder = new PromptBuilder();
-        builder.StartVoice(new CultureInfo(_languageCode));
+        builder.StartVoice(_voiceInfo);
         builder.AppendText(text);
         builder.EndVoice();
         _synthesizer.SpeakAsyncCancelAll();
