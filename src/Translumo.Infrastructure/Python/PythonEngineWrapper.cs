37ac61dc2692340fa9698529e80d29a5bfacb32e
Ivan <ivan@gubanov.cf>
2023-10-07 20:41:30 +0300

    update easy ocr to use python engine wrapper

--- src/Translumo.Infrastructure/Python/PythonEngineWrapper.cs
+++ src/Translumo.Infrastructure/Python/PythonEngineWrapper.cs
@@ -9,6 +9,7 @@ public class PythonEngineWrapper : IDisposable
 {
     private bool _disposedValue;
     private int _countUsage;
+    private IntPtr _threadState;
 
     public PythonEngineWrapper()
     {
@@ -66,23 +67,21 @@ public class PythonEngineWrapper : IDisposable
 
             if (PythonEngine.IsInitialized)
             {
-                //Causes PythonEngine.Shutdown() hanging (https://github.com/pythonnet/pythonnet/issues/1701)
-                //PythonEngine.EndAllowThreads(_threadState);
+                // Causes PythonEngine.Shutdown() hanging (https://github.com/pythonnet/pythonnet/issues/1701)
+                // PythonEngine.EndAllowThreads(_threadState);
                 PythonEngine.Shutdown();
             }
 
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            // TODO: set large fields to null
             _disposedValue = true;
         }
     }
 
-    //// TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-    //~PythonEngineWrapper()
-    //{
-    //    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-    //    Dispose(disposing: false);
-    //}
+    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~PythonEngineWrapper()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: false);
+    }
 
     public void Dispose()
     {
